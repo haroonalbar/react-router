288bfc83b45a987cbac1ca72c64eb344a58615f2
haroonalbar <haroonalbarx@gmail.com>
2023-04-28 13:39:05 +0530

    nested routing + added outlet on root

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,15 @@ const router = createBrowserRouter([{
   path: '/',
   element: <Root/>,
   errorElement: <ErrorPage/>, 
-},
+    children:[
+
   {
     path: "contacts/:contactId",
     element: <Contact />,
   },
+    ]
+},
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
