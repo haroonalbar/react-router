835154507095344029f7549599a807c9614b04b0
haroonalbar <haroonalbarx@gmail.com>
2023-04-30 20:57:05 +0530

    data fetching

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { getContacts } from "../contacts";
 
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 export default function Root() {
   return (
     <>
