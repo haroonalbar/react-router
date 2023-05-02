a024b58c67b10057acaf3c17adff78938700caee
haroonalbar <haroonalbarx@gamil.com>
2023-05-02 20:22:54 +0530

    changed button Form and added action to createContact  on route

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import Root, {loader as rootLoader} from './routes/root'
-import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 
-const router = createBrowserRouter([{
-  path: '/',
-  element: <Root/>,
-  errorElement: <ErrorPage/>, 
-  loader: rootLoader,
-    children:[
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
+const router = createBrowserRouter([
   {
-    path: "contacts/:contactId",
-    element: <Contact />,
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
-    ]
-},
+]);
 
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
-import { Outlet, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
-
+import { Outlet, useLoaderData, Form } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 export async function loader() {
   const contacts = await getContacts();
   return { contacts };
 }
 export default function Root() {
-  const {contacts} = useLoaderData();
+  const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
@@ -21,19 +24,15 @@ export default function Root() {
               type="search"
               name="q"
             />
-            <div
-              id="search-spinner"
-              aria-hidden
-              hidden={true}
-            />
-            <div
-              className="sr-only"
-              aria-live="polite"
-            ></div>
+            <div id="search-spinner" aria-hidden hidden={true} />
+            <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          {/* <form method="post">
             <button type="submit">New</button>
-          </form>
+          </form> */}
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
         </div>
         <nav>
           {/* <ul> */}
@@ -69,7 +68,7 @@ export default function Root() {
         </nav>
       </div>
       <div id="detail">
-        <Outlet/>
+        <Outlet />
       </div>
     </>
   );
